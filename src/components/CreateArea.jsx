faf23456de495168d5f37eb989ca57916854a4c9
Alessia Dallera <122983709+arale82@users.noreply.github.com>
2024-04-30 10:59:29 +0100

    add styles, icons and fab

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,7 +1,11 @@
 import React, {useState} from "react";
+import AddIcon from '@mui/icons-material/Add';
+import { Fab } from "@mui/material";
+import Zoom from '@mui/material/Zoom';
 
 function CreateArea(props) {
   const [newNote, setNewNote] = useState({});
+  const [isFormOpen, setFormOpen] = useState(false);
 
   function handleChange(event){
     const { name, value } = event.target;
@@ -12,18 +16,32 @@ function CreateArea(props) {
         [name] : value
       });
     });
+
+  }
+
+  function handleClick(){
+    setFormOpen(true);
   }
 
   return (
     <div>
-      <form onSubmit={(event) => {
-        props.onSubmit(newNote);
-        setNewNote({title : "", content : ""});
-        event.preventDefault();
-    }}>
-        <input value={newNote.title} onChange={handleChange} name="title" placeholder="Title" />
-        <textarea value={newNote.content} onChange={handleChange} name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+      <form className="create-note">
+        <Zoom in={isFormOpen}>
+          <input style={!isFormOpen && {display: "none"}} value={newNote.title} onChange={handleChange} name="title" placeholder="Title" />
+        </Zoom>
+        <textarea 
+          value={newNote.content}
+          onClick={handleClick} 
+          onChange={handleChange} name="content" placeholder="Take a note..." 
+          rows={(isFormOpen) ? "3" : "1"} />
+        <Zoom in={isFormOpen}>
+          <Fab onClick={(event) => {
+          props.onSubmit(newNote);
+          setNewNote({title : "", content : ""});
+      }}>
+            <AddIcon />
+          </Fab>
+        </Zoom>
       </form>
     </div>
   );
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import DeleteIcon from '@mui/icons-material/Delete';
+import { Fab } from "@mui/material";
 
 function Note(props) {
 
@@ -6,7 +8,7 @@ function Note(props) {
     <div className="note" id={props.id}>
       <h1>{props.title}</h1>
       <p>{props.content}</p>
-      <button onClick={() => {props.deleteButton(props.id);}}>Del</button>
+      <Fab onClick={() => {props.deleteButton(props.id);}}><DeleteIcon /></Fab>
     </div>
   );
 }
